1f1c6961b2e1c30444e513126a3a31c9d5adb79f
ShivamAgrawal04 <shivamagrawal0455@gmail.com>
2025-05-06 02:39:23 +0530

    fourth commit

--- frontend/src/context/axios.js
+++ frontend/src/context/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://barcoder1.onrender.com/api",
+  // baseURL: "http://localhost:5000/api",
   withCredentials: true,
 });
 
