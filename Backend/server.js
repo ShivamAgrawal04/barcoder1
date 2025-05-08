388a0871acbf69670a8b22155531d2437f035357
ShivamAgrawal04 <shivamagrawal0455@gmail.com>
2025-05-09 02:46:05 +0530

    the commit

--- Backend/server.js
+++ Backend/server.js
@@ -6,25 +6,25 @@ import { createServer } from "http";
 
 const server = createServer(app);
 
-const io = new Server(server, {
-  cors: {
-    origin: [
-      "http://localhost:3000",
-      "http://192.168.29.138:3000",
-      "https://barcoder1.vercel.app",
-    ],
-    credentials: true,
-  },
-});
-io.on("connection", (socket) => {
-  console.log("user connected User", socket.id);
+// const io = new Server(server, {
+//   cors: {
+//     origin: [
+//       "http://localhost:3000",
+//       "http://192.168.29.138:3000",
+//       "https://barcoder1.vercel.app",
+//     ],
+//     credentials: true,
+//   },
+// });
+// io.on("connection", (socket) => {
+//   console.log("user connected User", socket.id);
 
-  socket.on("join-shop-room", (shopId) => {
-    socket.join();
-    const menu = shopMenus[`shop_${shopId}`] || [];
-    socket.emit("menuUpdate", menu);
-  });
-});
+//   socket.on("join-shop-room", (shopId) => {
+//     socket.join();
+//     const menu = shopMenus[`shop_${shopId}`] || [];
+//     socket.emit("menuUpdate", menu);
+//   });
+// });
 
 server.listen(process.env.PORT, "0.0.0.0", () => {
   console.log("Server is running on port 5000");
