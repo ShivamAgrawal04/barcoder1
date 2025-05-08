651691bf404bacc630459eee917c150e25cab3b2
ShivamAgrawal04 <shivamagrawal0455@gmail.com>
2025-05-08 22:47:52 +0530

    the commit

--- frontend/src/components/QrCode.jsx
+++ frontend/src/components/QrCode.jsx
@@ -57,7 +57,7 @@ const QrCode = () => {
     <>
       <div ref={ref}></div>
       <button onClick={handleDownloadUpdatedQR}>
-        Update & Download QR anurag Helloooooooooooooooooooooooooooo
+        Update & Download QR anurag
       </button>
     </>
   );
