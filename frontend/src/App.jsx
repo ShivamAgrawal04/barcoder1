867cc501c5445e634c206e890ccbe449691cfdad
ShivamAgrawal04 <shivamagrawal0455@gmail.com>
2025-05-09 02:41:31 +0530

    the commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Nav from "./components/Navbar";
 import Footer from "./components/Footer";
 import SignUp from "./components/SignUp";
@@ -7,33 +7,30 @@ import ProductsAdd from "./components/ProductsAdd";
 import ProductList from "./components/ProductList";
 import UpdateProduct from "./components/UpdateProduct";
 import Private from "./components/PrivateComponent";
-import Qrcode from "./components/ProductQR";
 import ProductOnly from "./components/ProductOnly";
 import QrCode from "./components/QrCode";
 import PageNotFound from "./components/PageNotFound";
-import StyledQRCode from "./components/StyledQRCode";
 
 function App() {
+  const location = useLocation();
+  const isQRProductPage = location.pathname.startsWith("/qrproducts/");
   return (
     <div>
-      {/* Show Nav & Footer only for non-QR pages */}
-      {/* <Routes></Routes> */}
-      <Nav />
+      {!isQRProductPage && <Nav />}
       <Routes>
-        {/* <Route element={<Private />}> */}
-        <Route path="/" element={<ProductList />} />
-        <Route path="/add" element={<ProductsAdd />} />
-        <Route path="/update/:id" element={<UpdateProduct />} />
-        {/* </Route> */}
+        <Route element={<Private />}>
+          <Route path="/" element={<ProductList />} />
+          <Route path="/add" element={<ProductsAdd />} />
+          <Route path="/update/:id" element={<UpdateProduct />} />
+          <Route path="/qrcode" element={<QrCode />} />
+        </Route>
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
-        <Route path="/qrcode" element={<QrCode />} />
         <Route path="/qrproducts/:id" element={<ProductOnly />} />
-        <Route path="/qr" element={<StyledQRCode />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
 
-      <Footer />
+      {!isQRProductPage && <Footer />}
     </div>
   );
 }
