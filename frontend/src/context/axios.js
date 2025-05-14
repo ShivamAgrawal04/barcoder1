b6ea793ddf8df3c3d0e1b73c34c92ecece963ca8
ShivamAgrawal04 <shivamagrawal0455@gmail.com>
2025-05-14 14:05:30 +0530

    All things are completed

--- frontend/src/context/axios.js
+++ frontend/src/context/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  // baseURL: "https://barcoder1.onrender.com/api",
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://barcoder1.onrender.com/api",
+  // baseURL: "http://localhost:5000/api",
   // baseURL: "http://192.168.29.138:5000/api",
   withCredentials: true,
 });
