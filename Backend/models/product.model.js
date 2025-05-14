6a065457b024ccf0423bfa343dba236646dd6cec
ShivamAgrawal04 <shivamagrawal0455@gmail.com>
2025-05-14 14:28:29 +0530

    All things are completed

--- Backend/models/product.model.js
+++ Backend/models/product.model.js
@@ -9,7 +9,11 @@ const productSchema = new Schema(
     description: { type: String, lowercase: true },
     price: { type: Number, required: true },
     category: { type: String, lowercase: true },
-    productPic: { type: String, default: "" },
+    productPic: {
+      type: String,
+      default:
+        "https://res.cloudinary.com/epicplayers/image/upload/v1747212961/Barcoder/nz2napbkyx7gyjdnfpsa.png",
+    },
     availability: { type: Boolean, default: true },
   },
   { timestamps: true }
