45b36853fb7ef8838efbb3c3f8e99c0e827ee7c7
ShivamAgrawal04 <143934542+ShivamAgrawal04@users.noreply.github.com>
2025-05-16 10:24:11 +0530

    Update axios.js

--- frontend/src/context/axios.js
+++ frontend/src/context/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://barcoder1.onrender.com/api",
+  baseURL: "/api",
   // baseURL: "http://localhost:5000/api",
   // baseURL: "http://192.168.29.138:5000/api",
   withCredentials: true,
