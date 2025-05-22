c557e168e9cad2da34ffc99993d5741f9614bc45
ShivamAgrawal04 <shivamagrawal0455@gmail.com>
2025-05-23 03:36:10 +0530

    product only updated for laptop and mobile view

--- frontend/src/context/axios.js
+++ frontend/src/context/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  // baseURL: "/api",
-  baseURL: "http://localhost:5000/api",
+  baseURL: "/api",
+  // baseURL: "http://localhost:5000/api",
   // baseURL: "http://192.168.29.138:5000/api",
   withCredentials: true,
 });
