77e7ad8e0bf5f68e1b120bccd78a3ab3bc6aa2c2
ShivamAgrawal04 <shivamagrawal0455@gmail.com>
2025-05-23 01:43:46 +0530

    product only updated for laptop and mobile view

--- frontend/src/components/OptimizeProductList.jsx
+++ frontend/src/components/OptimizeProductList.jsx
@@ -9,11 +9,11 @@ const OptimizeProductList = () => {
 
   return (
     <div className="h-screen bg-gradient-to-br from-gray-900 via-slate-900 to-black  text-white overflow-hidden">
-      <div className="flex h-full">
+      <div className="flex h-full overflow-y-auto">
         {/* <div className="bg-[#424b61] h-full w-[18.7rem] p-[1.2rem] pb-24 shrink-0"> */}
         <LeftBar />
         {/* </div> */}
-        <div className="flex-1 overflow-y-auto p-4 no-scrollbar">
+        <div className="flex-1 overflow-y-auto p-4 no-scrollbar overflow-hidden">
           {/* <div className="pt-2">
             {location.pathname !== "/list" && <SearchBar />}
           </div> */}
--- frontend/src/components/OptimizeProductList1.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const OptimizeProductList1 = () => {
-  return <div>OptimizeProductList1</div>;
-};
-
-export default OptimizeProductList1;
